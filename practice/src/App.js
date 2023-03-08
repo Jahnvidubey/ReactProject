2237b51420c6f70d567796f5d5ed35b33547b7af
jahnvi dubey <jahnvi.dubey10@gmail.com>
2023-03-08 19:28:00 +0530

    a short description of the change

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import Header from "./MyComponents/header";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Material from './MyComponents/material';
 
 
 function App() {
   return (
     <>
-    <Header/>
-    <h2>its my first react</h2>
-    <Material/>
+    
     </>
   );
 }
